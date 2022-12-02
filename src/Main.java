b93e3cf21ed9c879987405e2d8803c55018fdf17
Pedroooliveria <pcoelho509@gmail.com>
2022-12-02 12:24:32 +0000

    Edgar ola

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+        int[] myArray = {1, 2, 3, 4, 5, 11, 7, 8, 9, 10};
+        Recursive recursive = new Recursive();
         System.out.println(Recursive.sumArray(myArray, myArray.length - 1));
-        System.out.println(Recursive.maxComumFatorial(20,10));
+        System.out.println(Recursive.maxComumFatorial(20, 5));
+        System.out.println(Recursive.maxNumber(myArray, myArray.length));
+        String string="pedro";
+        System.out.println(Recursive.numbOfC("Pedro",string.length()));
+
     }
 }
--- src/Recursive.java
+++ src/Recursive.java
@@ -15,4 +15,40 @@ public class Recursive {
         }
         return maxComumFatorial(num2, num1 % num2);
     }
+
+    public static int maxNumber(int[] array, int maxNum) {
+        if (maxNum == 1) {
+            return array[0];
+        }
+        if (array[0] < array[maxNum - 1]) {
+            array[0] = array[maxNum - 1];
+            return maxNumber(array, maxNum - 1);
+        }
+        return maxNumber(array, maxNum - 1);
+    }
+
+    public static int numbOfC(String string, int number) {
+        if (number == 1) {
+            if (!Character.toString(string.charAt(0)).equals("a") && !Character.toString(string.charAt(0)).equals("e") && !Character.toString(string.charAt(0)).equals("i") &&
+                    !Character.toString(string.charAt(0)).equals("o") && !Character.toString(string.charAt(0)).equals("u")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        if (!Character.toString(string.charAt(number - 1)).equals("a") && !Character.toString(string.charAt(number - 1)).equals("e") && !Character.toString(string.charAt(number - 1)).equals("i") &&
+                !Character.toString(string.charAt(number - 1)).equals("o") && !Character.toString(string.charAt(number - 1)).equals("u")) {
+            return numbOfC(string, number - 1) + 1;
+        } else {
+            return numbOfC(string, number - 1);
+        }
+    }
 }
+
+
+/*int count = 0;
+        if (Character.toString(name.charAt(count)).equals("a")) {
+            return sumConsoantes("ana" + count) - 1;
+        }
+
+        return 0;*/
